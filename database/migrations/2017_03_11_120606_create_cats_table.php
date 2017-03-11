e43403072b9dec01edf9e471c8250645c4a0432d
Marja Ahonen <ma415608@shell.sis.uta.fi>
2017-03-11 14:10:39 +0200

    Lisätty cat-luokka ja migraatiotaulu

---
+++ database/migrations/2017_03_11_120606_create_cats_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('creator')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cats');
+    }
+}
